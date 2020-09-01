1f4c273674413236b8a3f2f9ee9fda7bf23cc179
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-01 11:33:18 +0200

    Working graph demo

--- src/JavaClient.java
+++ src/JavaClient.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 public class JavaClient {
     DataOutputStream toServer = null;
     DataInputStream fromServer = null;
-    String host = "192.168.43.127";
+    String host = "192.168.1.6";
     int port = 65432;
 
     public static void main(String[] args) {
@@ -21,10 +21,18 @@ public class JavaClient {
                 toServer = new DataOutputStream(socket.getOutputStream());
                 fromServer = new DataInputStream(socket.getInputStream());
 
-                byte[] test = fromServer.readAllBytes();
+                // Get temperature
+                byte[] temp = fromServer.readAllBytes();
                 fromServer.close();
-                String string = new String(test, StandardCharsets.UTF_8);
-                System.out.println(string);
+                String tempStr = new String(temp, StandardCharsets.UTF_8);
+                System.out.println(tempStr);
+
+//                // Get humidity
+//                byte[] hum = fromServer.readAllBytes();
+//                fromServer.close();
+//                String humStr = new String(hum, StandardCharsets.UTF_8);
+//                System.out.println(humStr);
+
                 Thread.sleep(2000);
             }
         } catch (IOException | InterruptedException ex1) {
