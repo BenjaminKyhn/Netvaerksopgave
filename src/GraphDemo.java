49d7343368972692958b181b5c70f8c5e6929910
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-01 10:59:51 +0200

    Working graph demo

--- src/GraphDemo.java
+++ src/GraphDemo.java
@@ -1,180 +1,86 @@
-/* =========================
- * TimeSeriesChartDemo1.java
- * =========================
- *
- * (C) Copyright 2003-2017, by Object Refinery Limited.
- *
- * Project Info:  http://www.jfree.org/jfreechart/index.html
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   - Neither the name of the Object Refinery Limited nor the
- *     names of its contributors may be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL OBJECT REFINERY LIMITED BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.ui.ApplicationFrame;
-import org.jfree.chart.ui.RectangleInsets;
-import org.jfree.chart.ui.UIUtils;
-import org.jfree.data.time.*;
-import org.jfree.data.xy.XYDataset;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.time.Second;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 
 import javax.swing.*;
 import java.awt.*;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * An example of a time series chart create using JFreeChart.  For the most
- * part, default settings are used, except that the renderer is modified to
- * show filled shapes (as well as lines) at each data point.
- */
-public class GraphDemo extends ApplicationFrame {
+class GraphDemo {
+    // driver that actually runs the thing
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater( () -> {
+            LineChartEx ex = new LineChartEx();
+            ex.setVisible(true);
+        });
+    }
+}
 
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * A demonstration application showing how to create a simple time series
-     * chart.  This example uses monthly data.
-     *
-     * @param title the frame title.
-     */
-    public GraphDemo(String title) throws InterruptedException {
-        super(title);
-        ChartPanel chartPanel = (ChartPanel) createDemoPanel();
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        setContentPane(chartPanel);
+class LineChartEx extends JFrame {
+    public LineChartEx() {
+        initUI();
     }
 
-    /**
-     * Creates a chart.
-     *
-     * @param dataset a dataset.
-     * @return A chart.
-     */
-    private static JFreeChart createChart(XYDataset dataset) {
+    private void initUI() {
+        //create the series - add some dummy data
+        TimeSeries series1 = new TimeSeries("Temperatur");
+        TimeSeries series2 = new TimeSeries("Luftfugtighed");
 
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
-                "Temperatur og luftfugtighed",  // title
-                "Tid",             // x-axis label
-                "Grader",   // y-axis label
-                dataset);
+        for (int i = 0; i < 3; i++) {
+            Date date = new Date(2020, 9, 1,9,0,i*10);
+            Second second = new Second(date);
+            series1.add(second, i*1000);
+        }
 
+        for (int i = 0; i < 3; i++) {
+            Date date = new Date(2020, 9, 1,9,0,i*100);
+            Second second = new Second(date);
+            series2.add(second, i*10000);
+        }
+
+        //create the datasets
+        TimeSeriesCollection dataset1 = new TimeSeriesCollection();
+        TimeSeriesCollection dataset2 = new TimeSeriesCollection();
+        dataset1.addSeries(series1);
+        dataset2.addSeries(series2);
+
+        //construct the plot
+        XYPlot plot = new XYPlot();
+        plot.setDataset(0, dataset1);
+        plot.setDataset(1, dataset2);
+
+        //customize the plot with renderers and axis
+        plot.setRenderer(0, new XYSplineRenderer());//use default fill paint for first series
+        XYSplineRenderer splinerenderer = new XYSplineRenderer();
+        splinerenderer.setSeriesFillPaint(0, Color.BLUE);
+        plot.setRenderer(1, splinerenderer);
+        plot.setRangeAxis(0, new NumberAxis("Grader (°)(°)"));
+        plot.setRangeAxis(1, new NumberAxis("Procent (%)"));
+        plot.setDomainAxis(new DateAxis("Tid"));
+
+        //Map the data to the appropriate axis
+        plot.mapDatasetToRangeAxis(0, 0);
+        plot.mapDatasetToRangeAxis(1, 1);
+
+        //generate the chart
+        JFreeChart chart = new JFreeChart("Temperatur & Luftfugtighed", getFont(), plot, true);
         chart.setBackgroundPaint(Color.WHITE);
 
-        XYPlot plot = (XYPlot) chart.getPlot();
-        plot.setBackgroundPaint(Color.LIGHT_GRAY);
-        plot.setDomainGridlinePaint(Color.WHITE);
-        plot.setRangeGridlinePaint(Color.WHITE);
-        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
-        plot.setDomainCrosshairVisible(true);
-        plot.setRangeCrosshairVisible(true);
+        // NEW PART THAT MAKES IT WORK
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
 
-        XYItemRenderer r = plot.getRenderer();
-        if (r instanceof XYLineAndShapeRenderer) {
-            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
-            renderer.setDefaultShapesVisible(true);
-            renderer.setDefaultShapesFilled(true);
-            renderer.setDrawSeriesLineAsPath(true);
-        }
-
-        plot.setRangeAxis(0, new NumberAxis("Series 1"));
-        plot.setRangeAxis(1, new NumberAxis("Series 2"));
-
-        DateAxis axis = (DateAxis) plot.getDomainAxis();
-        axis.setDateFormatOverride(new SimpleDateFormat("hh:mm:ss"));
-
-        return chart;
-    }
-
-    /**
-     * Creates a dataset, consisting of two series of monthly data.
-     *
-     * @return The dataset.
-     */
-    private static XYDataset createDataset() throws InterruptedException {
-
-        TimeSeries s1 = new TimeSeries("Temperatur");
-
-        for (int i = 0; i < 30; i++) {
-            Date date = new Date(2020, 9, 1,9,0,i*2);
-            Second second = new Second(date);
-            s1.add(second, i * 15);
-        }
-
-        TimeSeries s2 = new TimeSeries("Luftfugtighed");
-        for (int i = 0; i < 30; i++) {
-            Date date = new Date(2020, 9, 1,9,0,i*2);
-            Second second = new Second(date);
-            s2.add(second, i + 15);
-        }
-
-        // ******************************************************************
-        //  More than 150 demo applications are included with the JFreeChart
-        //  Developer Guide...for more information, see:
-        //
-        //  >   http://www.object-refinery.com/jfreechart/guide.html
-        //
-        // ******************************************************************
-
-        TimeSeriesCollection dataset = new TimeSeriesCollection();
-        dataset.addSeries(s1);
-        dataset.addSeries(s2);
-
-        return dataset;
-
-    }
-
-    /**
-     * Creates a panel for the demo (used by SuperDemo.java).
-     *
-     * @return A panel.
-     */
-    public static JPanel createDemoPanel() throws InterruptedException {
-        JFreeChart chart = createChart(createDataset());
-        ChartPanel panel = new ChartPanel(chart, false);
-        panel.setFillZoomRectangle(true);
-        panel.setMouseWheelEnabled(true);
-        return panel;
-    }
-
-    /**
-     * Starting point for the demonstration application.
-     *
-     * @param args ignored.
-     */
-    public static void main(String[] args) throws InterruptedException {
-
-        GraphDemo demo = new GraphDemo(
-                "Temperatur og luftfugtighed");
-        demo.pack();
-        UIUtils.centerFrameOnScreen(demo);
-        demo.setVisible(true);
-
+        pack();
+        setTitle("Temperatur & Luftfugtighed");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
